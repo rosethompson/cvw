f994eedf255f57917a14f777b9ddfc5368656173
Rose Thompson <ross1728@gmail.com>
2024-09-10 16:45:17 -0700

    Multithreaded rvvidaemon running.  It's less efficient than the original verion, but that's ok because it does seem more stable.

---
+++ fpga/rvvidaemon/rvvidaemon-multithread.c
@@ -0,0 +1,401 @@
+///////////////////////////////////////////
+// rvvi daemon
+//
+// Written: Rose Thomposn ross1728@gmail.com
+// Created: Sept 10 2024
+// Modified: Sept 10 2024
+//
+// Purpose: Converts raw socket into rvvi interface to connect into ImperasDV
+// 
+// Documentation: Higher performance rvvi daemon
+// three threads.
+// T1 reads ethernet frames and stores into a queue
+// T2 runs the ImperasDV software.  It pulls instruction frames from the queue
+// T3 sends back error and maintenance messages to the FPGA
+//
+// A component of the CORE-V-WALLY configurable RISC-V project.
+// https://github.com/openhwgroup/cvw
+// 
+// Copyright (C) 2021-23 Harvey Mudd College & Oklahoma State University
+//
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+//
+// Licensed under the Solderpad Hardware License v 2.1 (the “License”); you may not use this file 
+// except in compliance with the License, or, at your option, the Apache License version 2.0. You 
+// may obtain a copy of the License at
+//
+// https://solderpad.org/licenses/SHL-2.1/
+//
+// Unless required by applicable law or agreed to in writing, any work distributed under the 
+// License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+// either express or implied. See the License for the specific language governing permissions 
+// and limitations under the License.
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <arpa/inet.h>
+#include <linux/if_packet.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netinet/ether.h>
+#include "rvviApi.h" // *** bug fix me when this file gets included into the correct directory.
+#include "idv/idv.h"
+
+
+#include "rvvidaemon.h"
+#include "queue.h"
+
+#define DEST_MAC0	0x43
+#define DEST_MAC1	0x68
+#define DEST_MAC2	0x11
+#define DEST_MAC3	0x11
+#define DEST_MAC4	0x02
+#define DEST_MAC5	0x45
+
+#define SRC_MAC0	0x54
+#define SRC_MAC1	0x16
+#define SRC_MAC2	0x00
+#define SRC_MAC3	0x00
+#define SRC_MAC4	0x54
+#define SRC_MAC5	0x8F
+
+#define BUF_SIZ		1024
+
+#define ETHER_TYPE	0x5c00  // The type defined in packetizer.sv
+
+#define DEFAULT_IF	"eno1"
+
+struct sockaddr_ll socket_address;
+uint8_t sendbuf[BUF_SIZ];
+struct ether_header *sendeh = (struct ether_header *) sendbuf;
+int tx_len = 0;
+int sockfd;
+
+void PrintInstructionData(RequiredRVVI_t *InstructionData);
+void * ReceiveLoop(void * arg);
+void * ProcessLoop(void * arg);
+int ProcessRvviAll(RequiredRVVI_t *InstructionData);
+void set_gpr(int hart, int reg, uint64_t value);
+void set_csr(int hart, int csrIndex, uint64_t value);
+void set_fpr(int hart, int reg, uint64_t value);
+int state_compare(int hart, uint64_t Minstret);
+
+int main(int argc, char **argv){
+  
+  if(argc != 2){
+    printf("Wrong number of arguments.\n");
+    printf("rvvidaemon <ethernet device>\n");
+    return -1;
+  }
+
+  int sockopt;
+  struct ifreq ifopts;	/* set promiscuous mode */
+  
+  /* Open RAW socket to receive frames */
+  if ((sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETHER_TYPE))) == -1) {
+    perror("socket");
+  }
+  printf("Here 0\n");
+
+  /* Set interface to promiscuous mode - do we need to do this every time? */
+  strncpy(ifopts.ifr_name, argv[1], IFNAMSIZ-1);
+  ioctl(sockfd, SIOCGIFFLAGS, &ifopts);
+  printf("Here 1\n");
+  ifopts.ifr_flags |= IFF_PROMISC;
+  ioctl(sockfd, SIOCSIFFLAGS, &ifopts);
+  printf("Here 2\n");
+  if (ioctl(sockfd, SIOCGIFINDEX, &ifopts) < 0)
+    perror("SIOCGIFINDEX");
+  
+  /* Allow the socket to be reused - incase connection is closed prematurely */
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof sockopt) == -1) {
+    perror("setsockopt");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
+  printf("Here 3\n");
+  
+  /* Bind to device */
+  if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, argv[1], IFNAMSIZ-1) == -1)	{
+    perror("SO_BINDTODEVICE");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
+  printf("Here 4\n");
+
+  /* Index of the network device */
+  socket_address.sll_ifindex = ifopts.ifr_ifindex;
+  /* Address length*/
+  socket_address.sll_halen = ETH_ALEN;
+  /* Destination MAC */
+  socket_address.sll_addr[0] = DEST_MAC0;
+  socket_address.sll_addr[1] = DEST_MAC1;
+  socket_address.sll_addr[2] = DEST_MAC2;
+  socket_address.sll_addr[3] = DEST_MAC3;
+  socket_address.sll_addr[4] = DEST_MAC4;
+  socket_address.sll_addr[5] = DEST_MAC5;
+
+  // queue to store incoming instructions
+  queue_t * InstructionQueue;
+  InstructionQueue = InitQueue(1024);
+
+    /* Construct the Ethernet header */
+  memset(sendbuf, 0, BUF_SIZ);
+  sendbuf[0] = DEST_MAC0;
+  sendbuf[1] = DEST_MAC1;
+  sendbuf[2] = DEST_MAC2;
+  sendbuf[3] = DEST_MAC3;
+  sendbuf[4] = DEST_MAC4;
+  sendbuf[5] = DEST_MAC5;
+  sendbuf[6] = SRC_MAC0;
+  sendbuf[7] = SRC_MAC1;
+  sendbuf[8] = SRC_MAC2;
+  sendbuf[9] = SRC_MAC3;
+  sendbuf[10] = SRC_MAC4;
+  sendbuf[11] = SRC_MAC5;
+
+  sendeh->ether_type = htons(ETHER_TYPE);
+  tx_len += sizeof(struct ether_header);
+  /* Packet data */
+  sendbuf[tx_len++] = 't';
+  sendbuf[tx_len++] = 'r';
+  sendbuf[tx_len++] = 'i';
+  sendbuf[tx_len++] = 'g';
+  sendbuf[tx_len++] = 'i';
+  sendbuf[tx_len++] = 'n';
+
+  sendeh->ether_type = htons(ETHER_TYPE);
+  tx_len += sizeof(struct ether_header);
+  /* Packet data */
+  sendbuf[tx_len++] = 't';
+  sendbuf[tx_len++] = 'r';
+  sendbuf[tx_len++] = 'i';
+  sendbuf[tx_len++] = 'g';
+  sendbuf[tx_len++] = 'i';
+  sendbuf[tx_len++] = 'n';
+
+  int i;
+  printf("buffer: ");
+  for(i=0;i<tx_len;i++){
+    printf("%02hhx ", sendbuf[i]);
+  }
+  printf("\n");
+  printf("sockfd %x\n", sockfd);
+  // imperasdv
+
+  if(!rvviVersionCheck(RVVI_API_VERSION)){
+    printf("Bad RVVI_API_VERSION\n");
+  }
+
+  rvviRefConfigSetString(IDV_CONFIG_MODEL_VENDOR, "riscv.ovpworld.org");
+  rvviRefConfigSetString(IDV_CONFIG_MODEL_NAME,"riscv");
+  rvviRefConfigSetString(IDV_CONFIG_MODEL_VARIANT, "RV64GC");
+  rvviRefConfigSetInt(IDV_CONFIG_MODEL_ADDRESS_BUS_WIDTH, 56);
+  rvviRefConfigSetInt(IDV_CONFIG_MAX_NET_LATENCY_RETIREMENTS, 6);
+
+  // eventually we want to put the elffiles here
+  rvviRefInit(NULL);
+  rvviRefPcSet(0, 0x1000);
+  
+  // Volatile CSRs
+  rvviRefCsrSetVolatile(0, 0xC00);   // CYCLE
+  rvviRefCsrSetVolatile(0, 0xB00);   // MCYCLE
+  rvviRefCsrSetVolatile(0, 0xC02);   // INSTRET
+  rvviRefCsrSetVolatile(0, 0xB02);   // MINSTRET
+  rvviRefCsrSetVolatile(0, 0xC01);   // TIME
+
+  // *** fix me
+  rvviRefCsrSetVolatile(0, 0x301);   // MISA
+  rvviRefCsrSetVolatile(0, 0xF13);   // mimpid
+
+
+  int iter;
+  for (iter = 0xC03; iter <= 0xC1F; iter++) {
+    rvviRefCsrSetVolatile(0, iter);   // HPMCOUNTERx
+  }
+  // Machine MHPMCOUNTER3 - MHPMCOUNTER31
+  for (iter = 0xB03; iter <= 0xB1F; iter++) {
+    rvviRefCsrSetVolatile(0, iter);   // MHPMCOUNTERx
+  }
+  // cannot predict this register due to latency between
+  // pending and taken
+  rvviRefCsrSetVolatile(0, 0x344);   // MIP
+  rvviRefCsrSetVolatile(0, 0x144);   // SIP
+
+  // set bootrom and bootram as volatile memory
+  rvviRefMemorySetVolatile(0x1000, 0x1FFF);
+  rvviRefMemorySetVolatile(0x2000, 0x3FFF);
+
+  // Privileges for PMA are set in the imperas.ic
+  // volatile (IO) regions are defined here
+  // only real ROM/RAM areas are BOOTROM and UNCORE_RAM
+  rvviRefMemorySetVolatile(0x2000000, 0x2000000 + 0xFFFF);
+  rvviRefMemorySetVolatile(0x10060000, 0x10060000 + 0xFF);
+  rvviRefMemorySetVolatile(0x10000000, 0x10000000 + 0x7);
+  rvviRefMemorySetVolatile(0x0C000000, 0x0C000000 + 0x03FFFFFF);
+  rvviRefMemorySetVolatile(0x00013000, 0x00013000 + 0x7F);
+  rvviRefMemorySetVolatile(0x10040000, 0x10040000 + 0xFFF);
+
+  // this loop will go into Thread T1
+
+  pthread_t ReceiveID, ProcessID;
+  pthread_create(&ReceiveID, NULL, &ReceiveLoop, (void *) InstructionQueue);
+  pthread_create(&ProcessID, NULL, &ProcessLoop, (void *) InstructionQueue);
+  //pthread_join(ReceiveID, NULL);
+  pthread_join(ProcessID, NULL);
+
+  //opSessionTerminate();
+  close(sockfd);
+  return 0;
+}
+
+void * ReceiveLoop(void * arg){
+  uint8_t buf[BUF_SIZ];
+  struct ether_header *eh = (struct ether_header *) buf;
+  ssize_t headerbytes, numbytes;
+  queue_t * InstructionQueue = (queue_t *) arg;
+  while(1) {
+    numbytes = recvfrom(sockfd, buf, BUF_SIZ, 0, NULL, NULL);
+    headerbytes = (sizeof(struct ether_header));
+    int result;
+    if (eh->ether_dhost[0] == DEST_MAC0 &&
+        eh->ether_dhost[1] == DEST_MAC1 &&
+        eh->ether_dhost[2] == DEST_MAC2 &&
+        eh->ether_dhost[3] == DEST_MAC3 &&
+        eh->ether_dhost[4] == DEST_MAC4 &&
+        eh->ether_dhost[5] == DEST_MAC5) {
+      RequiredRVVI_t *InstructionDataPtr = (RequiredRVVI_t *) (buf + headerbytes);
+
+      // now let's drive IDV
+      // start simple just drive and compare PC.
+      //printf("Before Enqueue\n");
+      Enqueue(InstructionDataPtr, InstructionQueue);
+      //printf("After Enqueue\n");
+      //PrintInstructionData(InstructionDataPtr);
+    }
+  }
+  pthread_exit(NULL);
+}
+
+void * ProcessLoop(void * arg){
+  queue_t * InstructionQueue = (queue_t *) arg;
+  RequiredRVVI_t  InstructionDataPtr;
+  int result;
+  while(1) {
+    if(!IsEmpty(InstructionQueue)){
+      //printf("Before Dequeue\n");
+      Dequeue(&InstructionDataPtr, InstructionQueue);
+      //printf("After Dequeue\n");
+      PrintInstructionData(&InstructionDataPtr);
+      result = ProcessRvviAll(&InstructionDataPtr);
+      if(result == -1) break;
+    }
+  }
+  pthread_exit(NULL);
+}
+
+int ProcessRvviAll(RequiredRVVI_t *InstructionData){
+  long int found;
+  uint64_t time = InstructionData->Mcycle;
+  uint8_t trap = InstructionData->Trap;
+  uint64_t order = InstructionData->Minstret;
+  int result;
+  int CSRIndex;
+
+  result = 0;
+  if(InstructionData->GPREn) set_gpr(0, InstructionData->GPRReg, InstructionData->GPRValue);
+  if(InstructionData->FPREn) set_fpr(0, InstructionData->FPRReg, InstructionData->FPRValue);
+  if(InstructionData->CSRCount > 0) {
+    for(CSRIndex = 0; CSRIndex < 3; CSRIndex++){
+      if(InstructionData->CSR[CSRIndex].CSRReg != 0){
+	set_csr(0, InstructionData->CSR[CSRIndex].CSRReg, InstructionData->CSR[CSRIndex].CSRValue);
+      }
+    }
+  }
+
+  if (trap) {
+    rvviDutTrap(0, InstructionData->PC, InstructionData->insn);
+  } else {
+    rvviDutRetire(0, InstructionData->PC, InstructionData->insn, 0);
+  }
+
+  if(!trap) result = state_compare(0, InstructionData->Minstret);
+  // *** set is for nets like interrupts  come back to this.
+  //found = rvviRefNetIndexGet("pc_rdata");
+  //rvviRefNetSet(found, InstructionData->PC, time);
+  return result;
+  
+}
+
+int state_compare(int hart, uint64_t Minstret){
+  uint8_t result = 1;
+  uint8_t stepOk = 0;
+  char buf[80];
+  rvviDutCycleCountSet(Minstret);
+  if(rvviRefEventStep(hart) != 0) {
+    stepOk = 1;
+    result &= rvviRefPcCompare(hart);
+    result &= rvviRefInsBinCompare(hart);
+    result &= rvviRefGprsCompare(hart);
+    result &= rvviRefFprsCompare(hart);
+    result &= rvviRefCsrsCompare(hart);
+  } else {
+    result = 0;
+  }
+
+  if (result == 0) {
+    /* Send packet */
+    if (sendto(sockfd, sendbuf, tx_len, 0, (struct sockaddr*)&socket_address, sizeof(struct sockaddr_ll)) < 0){
+      printf("Send failed\n");
+    }else {
+      printf("send success!\n");
+    }
+
+    sprintf(buf, "MISMATCH @ instruction # %ld\n", Minstret);
+    idvMsgError(buf);
+    return -1;
+  }
+  
+}
+
+void set_csr(int hart, int csrIndex, uint64_t value){
+  rvviDutCsrSet(hart, csrIndex, value);
+}
+
+void set_gpr(int hart, int reg, uint64_t value){
+  rvviDutGprSet(hart, reg, value);
+}
+
+void set_fpr(int hart, int reg, uint64_t value){
+  rvviDutFprSet(hart, reg, value);
+}
+
+
+ 
+void PrintInstructionData(RequiredRVVI_t *InstructionData){
+  int CSRIndex;
+  printf("PC = %lx, insn = %x, Mcycle = %lx, Minstret = %lx, Trap = %hhx, PrivilegeMode = %hhx",
+	 InstructionData->PC, InstructionData->insn, InstructionData->Mcycle, InstructionData->Minstret, InstructionData->Trap, InstructionData->PrivilegeMode);
+  if(InstructionData->GPREn){
+    printf(", GPR[%d] = %lx", InstructionData->GPRReg, InstructionData->GPRValue);
+  }
+  if(InstructionData->FPREn){
+    printf(", FPR[%d] = %lx", InstructionData->FPRReg, InstructionData->FPRValue);
+  }
+  if(InstructionData->CSRCount > 0) {
+    printf( ", Num CSR = %d", InstructionData->CSRCount);
+    for(CSRIndex = 0; CSRIndex < 3; CSRIndex++){
+      if(InstructionData->CSR[CSRIndex].CSRReg != 0){
+	printf(", CSR[%x] = %lx", InstructionData->CSR[CSRIndex].CSRReg, InstructionData->CSR[CSRIndex].CSRValue);
+      }
+    }
+  }
+  printf("\n");
+}
+
