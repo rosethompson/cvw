39e3f51177d8a79e0a7126550e89f1ee865b1f28
Rose Thompson <ross1728@gmail.com>
2024-10-03 15:23:03 -0500

    Updated buffer sizes.

--- fpga/rvvidaemon/rvvidaemon-multithread.c
+++ fpga/rvvidaemon/rvvidaemon-multithread.c
@@ -71,8 +71,8 @@
 
 #define DEFAULT_IF	"eno1"
 
-#define QUEUE_SIZE       4096
-#define QUEUE_THREASHOLD 64
+#define QUEUE_SIZE       16384
+#define QUEUE_THREASHOLD 1024
 
 struct sockaddr_ll socket_address;
 uint8_t sendbuf[BUF_SIZ];
